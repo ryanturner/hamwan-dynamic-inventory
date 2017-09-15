bce1083d7933a59031ca46aa258521810e560c0a
Ryan Turner <ryan.e.t@gmail.com>
2017-09-14 21:58:59 -0500

    Added manual port override 8080 for linux group

--- inventory.py
+++ inventory.py
@@ -5,17 +5,18 @@ import ssl
 
 # Use this like:
 # while sleep 30; do python prometheus/inventory.py https://portal.memhamwan.net/host/ansible.json linux > linux_hosts.json; done
-host_group = sys.argv[1]
-hosts = sys.argv[2]
+url = sys.argv[1]
+host_group = sys.argv[2]
 
 context = ssl._create_unverified_context()
-response = urllib2.urlopen(host_group, context=context)
+response = urllib2.urlopen(url, context=context)
 content = response.read()
 
 json_obj = json.loads(content)
 
 output_list = []
-if host_group === 'linux':
-    hosts = [s + ':8080' for s in hosts]
-output_list.append({'targets': json_obj[hosts], 'labels': {}})
+hosts = json_obj[host_group]
+if host_group == 'linux':
+    hosts = [s + ":8080" for s in hosts]
+output_list = [{'targets': hosts, 'labels': {}}]
 print json.dumps(output_list)
