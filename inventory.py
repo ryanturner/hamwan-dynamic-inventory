078e80e0ad428e5cf09e453de911c0bdd3b0d0af
Ryan Turner <ryan.e.t@gmail.com>
2017-09-14 21:27:14 -0500

    Make linux hosts have port 8080 added

--- inventory.py
+++ inventory.py
@@ -5,12 +5,17 @@ import ssl
 
 # Use this like:
 # while sleep 30; do python prometheus/inventory.py https://portal.memhamwan.net/host/ansible.json linux > linux_hosts.json; done
+host_group = sys.argv[1]
+hosts = sys.argv[2]
+
 context = ssl._create_unverified_context()
-response = urllib2.urlopen(sys.argv[1], context=context)
+response = urllib2.urlopen(host_group, context=context)
 content = response.read()
 
 json_obj = json.loads(content)
 
 output_list = []
-output_list.append({'targets': json_obj[sys.argv[2]], 'labels': {}})
+if host_group === 'linux':
+    hosts = [s + ':8080' for s in hosts]
+output_list.append({'targets': json_obj[hosts], 'labels': {}})
 print json.dumps(output_list)
